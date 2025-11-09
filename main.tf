e0cefc3fd09f66b615b3456f9a0c8472ef237066
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2025-11-09 18:30:14 +0900

    main.tfのスペルミスを修正

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
             source = "hashicorp/local"
             version = "~>2.0"
         }
-        rendom = {
+        random = {
             source = "hashicorp/random"
             version = "~>3.0"
         }
@@ -12,7 +12,7 @@ terraform {
 }
 
 # ランダムな文字列を生成
-resource "random_get" "server_name" {
+resource "random_pet" "server_name" {
     length = 2
     separator = "-"
 }
@@ -20,14 +20,14 @@ resource "random_get" "server_name" {
 # outputs/hello.txt というテキストファイルを作成
 resource "local_file" "example" {
     filename =  "${path.module}/outputs/hello.txt"
-    content = "Hello  name from Terraform\n Server name: ${random_get.server_name.id}\n Created at:${timestamp()}"
+    content = "Hello  name from Terraform\n Server name: ${random_pet.server_name.id}\n Created at:${timestamp()}"
 }
 
 # JSON設定ファイルを作成
 resource "local_file" "server_name"{
     filename = "${path.module}/outputs/congig.json"
     content = jsonencode({
-        server_name = random_get.server_name.id
+        server_name = random_pet.server_name.id
         enviroment = "develop"
         ports = [80, 443, 8080]
         enabled = true
